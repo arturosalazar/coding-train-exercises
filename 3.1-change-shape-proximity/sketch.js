f52331cbe10dea66bd6b2df6651f69aeebebf97f
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2018-12-26 23:44:55 -0600

    fix comments

--- 3.1-change-shape-proximity/sketch.js
+++ 3.1-change-shape-proximity/sketch.js
@@ -1,7 +1,8 @@
-//Exercise based on Coding Train 3.1
-//Goal: To use if() conditional statement
-//to control a central shape based on
-//the mouse position
+/*
+Exercise for Coding Train 3.1
+Goal: Use if() conditional statement to control a central shape 
+based on the mouse's proximity to shape
+*/
 
 function setup() {
   createCanvas(600, 400);
@@ -13,18 +14,18 @@ function draw() {
   stroke('white')
   noFill()
   
-  //circle
+  //circle if mouse to the left of center
   if (mouseX<200){
   	ellipse(300,200,100);
   }
   
-  //rectangle
+  //rectangle if mouse is at center/on the shape
   if (mouseX>200&&mouseX<400){
   	rectMode(CENTER);
     rect(300,200,100,100);
   }
   
-  //triangle
+  //triangle if mouse to the right of center
   if (mouseX>400&&mouseX<600){
   	triangle(300,150,250,250,350,250)
   }
