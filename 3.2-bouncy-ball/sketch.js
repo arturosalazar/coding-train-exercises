50e290f5afdd030af18a5ddba62b3d8dc962056b
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2018-12-28 18:50:53 -0600

    clean up code and comments

--- 3.2-bouncy-ball/sketch.js
+++ 3.2-bouncy-ball/sketch.js
@@ -1,14 +1,12 @@
 /*
   Exercise for Coding Train 3.2
-  Goal - make the ball bounce off each
-  side using if statements
+  Goal - make a ball bounce that bounces off each side using if statements
 */
 
 let x = 0;
 let y = 200;
 
 let speedX = 3; 
-// let speedY = -3;
 
 function setup() {
   createCanvas(600, 400);
@@ -16,21 +14,17 @@ function setup() {
 
 function draw() {
   background(0);
-  
   noFill();
   stroke(255);
   strokeWeight(4);
   ellipse (x,y,100,100);
 
+  //change the direction of change in x whenever hitting one or other edge
   if (x>width) {
-  	speedX = -speedX  
+  	speedX = -speedX;
   }
-  
   if (x<0) {
-  	speedX = -speedX  
+  	speedX = -speedX;  
   }
-  
-  x = x + speedX
-  
-  
+  x = x + speedX;  
 }
