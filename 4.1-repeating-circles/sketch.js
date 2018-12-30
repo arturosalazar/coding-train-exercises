58a9455c023107558e52420368a9dbb9f0fe68a9
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2018-12-30 12:55:21 -0600

    change colors and shapes depending on loc

--- 4.1-repeating-circles/sketch.js
+++ 4.1-repeating-circles/sketch.js
@@ -2,6 +2,8 @@
   Exercise for Coding Train 4.1
   Modification of previous version of exercise
   Use a for loop to create circles across the canvas (instead of while)
+  Use map to make the colors change depending on where the shape is created across the canvas
+  Make every other shape a rectangle
 */
 
 function setup() {
@@ -10,9 +12,17 @@ function setup() {
 
 function draw() {
   background(0);
-  strokeWeight(4);
-  stroke(255)
+ 	noStroke()
+  let blue = 0;
   for (x = 0; x <= width; x = x + 50) {
-    ellipse(x, 200, 25, 25);
+    blue = map(x,0,width,0,255);
+    fill(255,0,blue);
+    if(x%100==50){
+			rectMode(CENTER)
+      rect(x,200,25,25)
+    }
+    else{
+      ellipse(x, 200, 25, 25);
+    }
   }
 }
