3d3755a9c07faff33961165b48b91ef534c1218c
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2018-12-30 12:36:49 -0600

    update to toggle between backgrounds
    Update sketch to use "on" variable that will toggle between background on/off instead of requiring the user to hold down the mouse key to change the background color

--- 3.4-highlight-rectangle/sketch.js
+++ 3.4-highlight-rectangle/sketch.js
@@ -3,29 +3,39 @@
   Exercise for Coding Train 3.4
   Goal - Create a rectangle, use if statements to highlight
   rectangle when hovering mouse over it
-  Use mouseIsPressed variable to change the background only if
-  the mouse is hovering over the rectangle
+  Use mousePressed function and on variable to toggle between backgrounds
+  only if mouse clicked while hovering over the rectangle
 */
+
+let on = false;
 
 function setup() {
   createCanvas(600, 400);
 }
 
 function draw() {
-  background(0);
+  //determine background color using on variable
+  if (on) {
+    background(0, 255, 0)
+  } else {
+    background(0);
+  }
 
   noFill()
   stroke(255)
   strokeWeight(4)
 
-  //only work if mouse is within all 4 sides of the rectangle
+  //only highlight rectangle if mouse is within all 4 sides of the rectangle
   if (mouseX > 246 && mouseX < 354 && mouseY > 146 && mouseY < 254) {
-    if(mouseIsPressed){
-      background(0,255,0)
-    }
-    fill(255, 0, 0)
+    fill(255, 0, 200)
   }
-
   rectMode(CENTER)
   rect(300, 200, 100, 100)
+}
+
+//if mouse clicked while hovering, change on variable
+function mousePressed() {
+  if (mouseX > 246 && mouseX < 354 && mouseY > 146 && mouseY < 254) {
+    on = !on
+  }
 }
