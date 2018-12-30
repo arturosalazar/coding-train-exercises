180dcce766850cc5f71fd0423efd3bc91599caff
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2018-12-30 12:44:40 -0600

    Include check for hovering in own function
    Reduce code by having the check for hover in one function. This function will modify a variable that will tell the other functions whether the rectangle is being hovered over

--- 3.4-highlight-rectangle/sketch.js
+++ 3.4-highlight-rectangle/sketch.js
@@ -1,19 +1,22 @@
 
 /*
   Exercise for Coding Train 3.4
-  Goal - Create a rectangle, use if statements to highlight
-  rectangle when hovering mouse over it
-  Use mousePressed function and on variable to toggle between backgrounds
-  only if mouse clicked while hovering over the rectangle
+  Goal - Create a rectangle, create checkHover function that determines if the mouse is hovering over rectangle
+  Toggle between backgrounds only if mouse clicked while hovering over the rectangle
+  
+  Using a function to check if hovering will reduce duplicate code 
+  by containing the check in one function for use in multiple areas
 */
 
 let on = false;
+let hoveringOnRect = false;
 
 function setup() {
   createCanvas(600, 400);
 }
 
 function draw() {
+  checkHover();
   //determine background color using on variable
   if (on) {
     background(0, 255, 0)
@@ -25,17 +28,24 @@ function draw() {
   stroke(255)
   strokeWeight(4)
 
-  //only highlight rectangle if mouse is within all 4 sides of the rectangle
-  if (mouseX > 246 && mouseX < 354 && mouseY > 146 && mouseY < 254) {
+  //only work if mouse is within all 4 sides of the rectangle
+  if (hoveringOnRect) {
     fill(255, 0, 200)
   }
   rectMode(CENTER)
   rect(300, 200, 100, 100)
 }
 
-//if mouse clicked while hovering, change on variable
+//if mouse clicked while hovering, change background variable
 function mousePressed() {
-  if (mouseX > 246 && mouseX < 354 && mouseY > 146 && mouseY < 254) {
+  if (hoveringOnRect) {
     on = !on
   }
+}
+
+function checkHover(){
+	if (mouseX > 246 && mouseX < 354 && mouseY > 146 && mouseY < 254) {
+    hoveringOnRect = true;
+  }
+  else hoveringOnRect = false;
 }
