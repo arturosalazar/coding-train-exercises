da5180e97ce0f57eaaec7ab4c43da1ae5b29c4c8
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2018-12-31 00:26:11 -0600

    update to turn on/off bubbles

--- 7.4-random-bubbles-class/sketch.js
+++ 7.4-random-bubbles-class/sketch.js
@@ -2,19 +2,18 @@
   Copy of the code from Coding Train 7.4 UPDATED VER 10/17
   Practice mouse interaction
 
-  Goal - create bubbles that print "Clicked!" to the console if clicked
+  Update so that the bubble's fill will turn off and on if that bubble is clicked
 */
-
 
 let bubbles = [];
 
 function setup() {
   createCanvas(600, 400);
-  for (let i = 0; i<5; i++){
-  	let x = random(width);
+  for (let i = 0; i < 5; i++) {
+    let x = random(width);
     let y = random(height);
-    let r = random(10,50);
-    let b = new Bubble(x,y,r);
+    let r = random(10, 50);
+    let b = new Bubble(x, y, r);
     bubbles.push(b);
   }
 }
@@ -22,8 +21,8 @@ function setup() {
 //when mouse pressed, run a function on the bubble
 //function will check if the bubble is pressed
 function mousePressed() {
-	for (let i=0;i<bubbles.length;i++){
-  	bubbles[i].clicked(mouseX,mouseY);
+  for (let i = 0; i < bubbles.length; i++) {
+    bubbles[i].clicked(mouseX, mouseY);
   }
 }
 
@@ -34,20 +33,24 @@ function draw() {
     bubbles[i].move();
     bubbles[i].show();
   }
-  
 }
 
 class Bubble {
   constructor(x, y, r) {
-    this.x = x
-    this.y = y
-    this.r = r
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.brightness = 0;
   }
   //checks if bubble clicked, if so will display console msg
-  clicked(px,py){
-  	let d = dist(px,py,this.x,this.y)
-    if (d<this.r){
-    	console.log("Clicked!!!!") 
+  clicked(px, py) {
+    let d = dist(px, py, this.x, this.y)
+    if (d < this.r) {
+      if (this.brightness === 0) {
+        this.brightness = 255;
+      } else {
+        this.brightness = 0;
+      }
     }
   }
   move() {
@@ -57,7 +60,7 @@ class Bubble {
   show() {
     stroke(255);
     strokeWeight(4);
-    noFill();
+    fill(this.brightness, 100);
     ellipse(this.x, this.y, this.r * 2);
   }
 }
