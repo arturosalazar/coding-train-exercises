5ba46ce55b5f26a044cf14069139966691a4b441
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2018-12-30 23:38:43 -0600

    Update to create a smokey effect

--- 7.3-random-bubbles-class/sketch.js
+++ 7.3-random-bubbles-class/sketch.js
@@ -2,14 +2,14 @@
   Copy of the code from Coding Train 7.3 UPDATED VER 10/17
   Practice using for loops to create objects in arrays utilizing Class definitions
   
-  Goal - create bubbles using class definitions and "new" keyword  
+  Goal - create bubbles using class definitions and "new" keyword 
+  Update the class to give a smokey effect - white color with a small alpha (almost transparent)
 */
-
 let bubbles = [];
 
 function setup() {
   createCanvas(600, 400);
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 1000; i++) {
     bubbles[i] = new Bubble()
   }
 }
@@ -28,16 +28,15 @@ class Bubble {
   constructor() {
     this.x = random(0, width);
     this.y = random(0, height);
-    this.r = random(10, 40);
+    this.r = random(10,40);
   }
   move() {
     this.x = this.x + random(-5, 5);
     this.y = this.y + random(-5, 5);
   }
-  show() {
-      stroke(255);
-      strokeWeight(4);
-      noFill();
-      ellipse(this.x, this.y, this.r*2);
-    }
+  show() { 	
+    noStroke()
+    fill(255,10);
+    ellipse(this.x, this.y, this.r*2);
+  }
 }
