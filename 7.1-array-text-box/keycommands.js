2942029cbec66da43404bd619d804de118c49252
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2018-12-30 22:20:01 -0600

    Add files via upload

---
+++ 7.1-array-text-box/keycommands.js
@@ -0,0 +1,32 @@
+function mousePressed() {
+  position += 1;
+  if (position === 4) {
+    position = 0;
+  }
+}
+
+function keyPressed() {
+  //spacebar to decriment
+  if (keyCode == 32) {
+    sizeNum = Math.floor(random(0, 3));
+    position -= 1;
+    if (position < 0) {
+      position = 3;
+    }
+  }
+  //decriment left arrow
+  if (keyCode == 37) {
+    position -= 1;
+    if (position < 0) {
+      position = 3;
+    }
+  }
+  //increment right arrow
+  if (keyCode == 39) {
+    position += 1;
+    if (position === 4) {
+      position = 0;
+    }
+  }
+
+}
--- 7.1-array-text-box/sketch.js
+++ 7.1-array-text-box/sketch.js
@@ -1,14 +1,14 @@
-/*
-  Exercise for Coding Train 7.1
+//Exercise for Coding Train 7.1 - What is an array
 
-  Goal - recreate the demonstration from the youtube video
-  
-  Create an array with strings. Display one of the string values to the screen
-  
-  Iterate over the array values (circling to the first item after the last) when the mouse is clicked
-*/
+//Goal - create own array to use on a different parameter
 
-let words = ["swing dancing","lindy hop","balboa","charleston"]
+//try to make the parameter random
+
+let words = ["swing dancing", "lindy hop", "balboa", "charleston"]
+
+let size = [32, 12, 60, 40]
+
+let sizeNum = 0;
 
 let position = 0;
 
@@ -18,15 +18,8 @@ function setup() {
 
 function draw() {
   background(0);
-  fill(255);
-  textSize(32);
-  text(words[position],12,200);
+  fill(255)
+  textSize(size[sizeNum])
+  text(words[position], 12, 200);
 }
 
-function mousePressed(){
-  position +=1;
-  if (position === 4){
-  	position = 0;
-  }
-	
-}
