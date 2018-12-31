5af2e971b050912b99b61e640a97751a9b16c4b2
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2018-12-31 00:06:54 -0600

    modification to create new objects on click
    space bar and mouse will now generate a new forest of trees or single tree, respectively

--- 7.3-happy-tree/userinput.js
+++ 7.3-happy-tree/userinput.js
@@ -1,18 +1,27 @@
+//Mod to create new trees when mouse is pressed, and to generate a whole new bunch when spacebar hit
+
 //reset when hitting space bar
 function keyPressed() {
   if (keyCode === 32) {
+    for (i = 0; i < 60 ; i++) {
+      let x = random (0,200);
+    	let y = random (0,150);
+    	let width = 30;
+    	let speed = random(2,10);
+  		let tree = new HappyTree(x,y,width,speed);
+      trees.push(tree); 
+    } 
+  }
+  if (keyCode === 38) {
     for (i = 0; i < trees.length; i++) {
-      trees[i].x = random(0,100)
-      trees[i].y = random(0,100)
-      trees[i].speed = random(1,5);
+      trees[i].speed += 2;
     }
   }
 }
 
-//when the mouse is pressed, increase tree speed
-//"sprint to finish"
+
 function mousePressed() {
-  for (i = 0; i < trees.length; i++) {
-    trees[i].speed += 2;
-  }
+  let speed = random (2,10)
+	let tree = new HappyTree (mouseX,mouseY,30,speed)
+  trees.push(tree);
 }
