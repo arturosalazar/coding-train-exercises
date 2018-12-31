b64e237140e94bebe52cf80f360303667acf6e0d
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2018-12-30 23:49:01 -0600

    create new objects/circles when mouse is clicked
    Update so the random circles are created at the mouse location when the mouse is clicked

--- 7.3-random-bubbles-class/sketch.js
+++ 7.3-random-bubbles-class/sketch.js
@@ -3,15 +3,19 @@
   Practice using for loops to create objects in arrays utilizing Class definitions
   
   Goal - create bubbles using class definitions and "new" keyword 
-  Update the class to give a smokey effect - white color with a small alpha (almost transparent)
+  Create circles on mouse click
 */
 let bubbles = [];
 
 function setup() {
   createCanvas(600, 400);
-  for (let i = 0; i < 1000; i++) {
-    bubbles[i] = new Bubble()
-  }
+}
+
+//when mouse clicked, create a new object in a variable and push that variable to the array
+function mousePressed() {
+  let r = random(10, 40);
+  let b = new Bubble(mouseX, mouseY, r);
+  bubbles.push(b);
 }
 
 function draw() {
@@ -25,18 +29,19 @@ function draw() {
 
 
 class Bubble {
-  constructor() {
-    this.x = random(0, width);
-    this.y = random(0, height);
-    this.r = random(10,40);
+  constructor(x, y, r) {
+    this.x = x
+    this.y = y
+    this.r = r
   }
   move() {
     this.x = this.x + random(-5, 5);
     this.y = this.y + random(-5, 5);
   }
-  show() { 	
-    noStroke()
-    fill(255,10);
-    ellipse(this.x, this.y, this.r*2);
+  show() {
+    stroke(255);
+    strokeWeight(4);
+    noFill();
+    ellipse(this.x, this.y, this.r * 2);
   }
 }
