21715d9c425b6bf6c6f831b65c029133cb1f474a
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2019-01-01 16:22:40 -0600

    Update the comments and clean up formatting

--- 7.6-interacting-bubbles/sketch.js
+++ 7.6-interacting-bubbles/sketch.js
@@ -1,15 +1,15 @@
-//Copy of the code from Coding Train 7.6 
-//Object communication part 1 
+/*
+Copy of the code from Coding Train 7.6 - Object communication part 1 
 
-//goal - recreate code from 7.6
-// 2 bubbles
-// create function in bubble class to detect
-// if 2 bubbles touch
+goal - recreate code from 7.6
+create a bubble class - within the bubble class, create a function to detect if 2 bubbles touch
+create two bubbles with the class
 
-// if so, make them do something 
+change the background if the two bubbles touch
+*/
 
-let bubble1
-let bubble2
+let bubble1;
+let bubble2;
 
 function setup() {
   createCanvas(600, 400);
@@ -17,15 +17,13 @@ function setup() {
   bubble2 = new Bubble(300,300,100);
 }
 
-
 function draw() {
   background(0);
 	
   if (bubble1.intersects(bubble2)){
    background(100,0,250); 
   }
-  
-  
+ 
   bubble1.show();
   bubble2.show();
   bubble1.move();
@@ -34,30 +32,24 @@ function draw() {
   bubble2.y = mouseY;
 }
 
-
 class Bubble {
   constructor(x, y, r=50) {
     this.x = x;
     this.y = y;
     this.r = r;
   }
-
-	intersects(other){
-    let d = dist(this.x,this.y,other.x,other.y)
-    return d<this.r+other.r
+  intersects(other){
+    let d = dist(this.x,this.y,other.x,other.y);
+    return d<this.r+other.r;
   }
-
-
-
   move() {
     this.x = this.x + random(-1, 1);
     this.y = this.y + random(-1, 1);
   }
-
   show() {
     stroke(255);
     strokeWeight(4);
-    noFill()
+    noFill();
     ellipse(this.x, this.y, this.r * 2);
   }
 }
