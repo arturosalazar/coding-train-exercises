1bfe0e1cde710dc0c079ce9b8d33aa8a73958baa
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2019-01-02 23:33:02 -0600

    Update sketch.js

--- 7.8-generating-kittens/sketch.js
+++ 7.8-generating-kittens/sketch.js
@@ -1,13 +1,11 @@
-//Exercise for Coding Train 7.8 - Objects
-//and images
+/*
+  Exercise for Coding Train 7.8 - Objects and images
 
-//using the previous bubble object, generate
-//objects that will display images
+  Goal - using the previous bubble object from 7.7, generate objects that will display images
 
-//display images randomly and allow
-//user to click an image to change it
+  Display images randomly and allow user to click an image to change it
+*/
 
-//This is used to create the objects to disp image
 let bubbles = [];
 
 //This is just used to hold on to the images
@@ -19,7 +17,6 @@ function preload(){
   }
 }
 
-
 function setup() {
   createCanvas(600, 400);
   for (let i = 0; i<20; i++){
@@ -27,7 +24,7 @@ function setup() {
     let y = random(height);
     let r = random(10,50);
     let kitten = random(kittens);
-  	b = new Bubble(x,y,r, kitten);
+    b = new Bubble(x,y,r, kitten);
     bubbles.push(b);
   }
 }
@@ -39,29 +36,12 @@ function mousePressed() {
 }
 
 function draw() {
-  background(0);
-	
+  background(0);	
   for (let i = 0; i<bubbles.length; i++){
-  	bubbles[i].show();
+    bubbles[i].show();
     bubbles[i].move();
-    
-    // for (let j = 0; j<bubbles.length; j++) {
-  	// //We check if b doesn't equal other to 
-    // //avoid an object checking if it 
-    // //intersects itself
-    // 	if (bubbles[i]!==bubbles[j]&&bubbles[i].intersects(bubbles[j])){
-		// 		bubbles[i].overlapping = true;
-    //   }
-    // if (bubbles[i].overlapping) {
-    // 	bubbles[i].changeColor(255);
-    // } else {
-    //   bubbles[i].changeColor(0);
-    // }
-    //}
   }
-  
 }
-
 
 class Bubble {
   constructor(x, y, r, kitten) {
@@ -70,24 +50,19 @@ class Bubble {
     this.r = r;
     this.kitten = kitten;
   }
-
-	intersects(other){
-    let d = dist(this.x,this.y,other.x,other.y)
-    return d<this.r+other.r
+  intersects(other){
+    let d = dist(this.x,this.y,other.x,other.y);
+    return d<this.r+other.r;
   }
-
   clicked(px, py) {
-   if (px > this.x && px < this.x + this.r && py > this.y && py < this.y + this.r) {
-     this.kitten = random(kittens);
-   }
- }
-
-
+    if (px > this.x && px < this.x + this.r && py > this.y && py < this.y + this.r) {
+      this.kitten = random(kittens);
+    }
+  }
   move() {
     this.x = this.x + random(-1, 1);
     this.y = this.y + random(-1, 1);
   }
-
   show() {
     image(this.kitten, this.x, this.y, this.r, this.r);
   }
